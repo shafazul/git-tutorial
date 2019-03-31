d3674d2e8b99cd0db25bb22f88f037bab309c4f4
shafazul <safajul52@gmail.com>
2019-03-30 18:54:33 -0700

    create change methode

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello{
         system.out.println("vs code Awesome");
        
     }
-   public static void sqr(int x) {
-       return x*x;
+   public static void display(int x) {
+       system.out.println("Merge conflict ");
 }
